66a9a8887f308c6b91dd569dca4caa5a001eb40c
Jesus <jjadb@hotmail.com>
2016-02-19 17:51:25 +0100

    Login hecho

--- TodoTestJSF-war/src/java/todotest/bean/LoginBean.java
+++ TodoTestJSF-war/src/java/todotest/bean/LoginBean.java
@@ -5,21 +5,57 @@
  */
 package todotest.bean;
 
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import todotest.ejb.UsuarioFacade;
+import todotest.entities.Usuario;
 
 /**
  *
- * @author andresbailen93
+ * @author inftel23
  */
-@Named(value = "loginBean")
-@Dependent
+@ManagedBean
+@RequestScoped
 public class LoginBean {
+    @EJB
+    private UsuarioFacade usuarioFacade;
+    
+    private String dni = "33333333P";
+    private String password = "1234";
 
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     /**
      * Creates a new instance of LoginBean
      */
     public LoginBean() {
+    }
+    
+    public String doLogin(){
+        Usuario user = usuarioFacade.find(this.dni);
+        if (user == null)
+            return "index";
+        if (!user.getPassword().equals(this.password))
+            return "index";
+        if (user.getEsProf() == 1)
+            return "mainPageTeacher";
+        else
+            return "mainPageStudent";
     }
     
 }
